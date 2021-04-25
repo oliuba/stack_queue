f883a6bba32e7ec01abba05270fd985399d5f9e6
Olha Liuba <olgaluba3@gmail.com>
2021-04-25 23:55:01 +0300

    Remove tests

--- queue_to_stack.py
+++ queue_to_stack.py
@@ -13,22 +13,3 @@ def queue_to_stack(queue: ArrayQueue) -> ArrayStack:
     for elem in values:
         stack.push(elem)
     return stack
-
-queue = ArrayQueue()
-for i in range(10):
-    queue.add(i)
-stack = queue_to_stack(queue)
-print(queue)
-# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(stack)
-# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-print(stack.pop())
-
-print(queue.pop())
-
-stack.add(11)
-queue.add(11)
-print(queue)
-# [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
-print(stack)
-# [9, 8, 7, 6, 5, 4, 3, 2, 1, 11]
